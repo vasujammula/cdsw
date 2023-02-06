1a677ddb38956c95ef5a4e6c6c8b128aeee9f061
vasujammula <vasu1824@gmail.com>
2023-02-06 12:51:27 +0530

    Create impala.py

---
+++ impala.py
@@ -0,0 +1,61 @@
+# Install Libraries
+!pip3 install impyla==0.17.0 --proxy http://pxuser:squidpassword@squid.central-tools.infra.cloudera.com:3128
+!pip3 install thrift==0.11.0 --proxy http://pxuser:squidpassword@squid.central-tools.infra.cloudera.com:3128
+!pip3 install thrift_sasl==0.4.3 --proxy http://pxuser:squidpassword@squid.central-tools.infra.cloudera.com:3128
+!pip3 install --upgrade kerberos>=1.3.0 --proxy http://pxuser:squidpassword@squid.central-tools.infra.cloudera.com:3128
+!pip install pandas
+
+import os
+import pandas
+import re
+from impala.dbapi import connect
+from impala.util import as_pandas
+
+# Connect to Impala
+for i in ['cml-pvc-prxm-3.cml-pvc-prxm.root.hwx.site','cml-pvc-prxm-1.cml-pvc-prxm.root.hwx.site','cml-pvc-prxm-2.cml-pvc-prxm.root.hwx.site','cml-pvc-prxm-4.cml-pvc-prxm.root.hwx.site']:
+  try:
+    
+    conn = connect(host=i,
+        port=21050,
+        auth_mechanism='GSSAPI',
+        use_ssl=True,
+        kerberos_service_name='impala')
+  except Exception as e :
+    print("NOT ABLE TO CONNECT TO:",i)
+
+# Execute using SQL
+cursor = conn.cursor()
+
+cursor.execute('show databases')
+db = as_pandas(cursor)
+print(db)
+
+cursor.execute('show tables')
+tables = as_pandas(cursor)
+print(tables)
+
+cursor.execute('CREATE TABLE IF NOT EXISTS Persons (PersonID int,Name STRING,City STRING);')
+create_table = as_pandas(cursor)
+print(create_table)
+
+cursor.execute('describe Persons')
+desc_table = as_pandas(cursor)
+print(desc_table)
+
+#cursor.execute('DROP TABLE Persons')
+#drop_table = as_pandas(cursor)
+#print(drop_table)
+
+cursor.execute('SELECT * FROM Persons')
+table_content = as_pandas(cursor)
+print(table_content)
+
+insert_data = 'INSERT INTO Persons (personid, name, city) VALUES (%s, %s, %s)'
+cursor.execute(insert_data, (1, 'A', 'abc'))
+
+insert_data = 'INSERT INTO Persons (personid, name, city) VALUES (%s, %s, %s)'
+cursor.execute(insert_data, (1, 'B', 'def'))
+
+cursor.execute('SELECT * FROM Persons')
+table_content = as_pandas(cursor)
+print(table_content)
